c8ba0a95c35887da886a23b2fc0daf0f4e452508
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-26 10:46:50 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
         />
         <h1 className="bold-52 lg:bold-88">Baixar WangaChat IA</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
+          Quer respostas rápidas, inteligentes e sempre disponíveis? A WangaChat é a sua assistente virtual criada para ajudar no dia-a-dia: tirar dúvidas, aprender, pesquisar, conversar, estudar e até criar conteúdos — tudo com a rapidez de uma IA moderna.
         </p>
 
         <div className="my-11 flex flex-wrap gap-5">
